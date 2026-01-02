e90c6ff8c2053e2242e1963c433ef3ffa533bc68
Sneha Saha <souvik9874721150@gmail.com>
2026-01-02 23:55:47 +0530

    VARIABLES AND DATA TYPES

---
+++ 2nd(Variables).py
@@ -0,0 +1,30 @@
+## VARIABLES AND DATA TYPES IN PYTHON
+
+# Variables are like boxes where we store data
+
+"""Rules:
+  . We can not start variable name with a number (1name = "Ram" is incorrect) 
+     and a symbol (#name = "Ram" is incorrect) in symbol we only use '_' (_name = "Ram" is correct),
+     we also use numbers but after one charecter (n1name = "Ram" is correct)
+  . We could not gave any gap in variable name (na me = "Ram" is incorrect)"""
+
+
+''' We use # for sigle line comment
+    We use """ """ or ''' ''' for multiline comment'''
+
+name = "Ram"
+n1ame = "Shyam"
+_name = "Hari"
+age = 25
+height = 5.9
+
+print(name)  # => Ram
+print(n1ame)  # => Shyam
+print(_name)  # => Hari
+print(age)  # => 25
+print(height) # => 5.9
+
+"""1name = "Kiran"  # This will give error
+   *name = "Sita"  # This will give error
+   na me = "Gita"  # This will give error"""
+
