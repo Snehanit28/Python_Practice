c830c5c3c0d4ddc07ed1c2407f23942a4628829c
Sneha Saha <souvik9874721150@gmail.com>
2026-01-10 23:47:45 +0530

    Comparison Operators

---
+++ Operators2(Comparison Operators).py
@@ -0,0 +1,19 @@
+# Comparison Operators: Used to compare two values and return a boolean result (True or False).
+
+"""Comparison Operators:
+     ==  -> Equal to
+     !=  -> Not equal to
+     >   -> Greater than
+     <   -> Less than
+     >=  -> Greater than or equal to
+     <=  -> Less than or equal to"""
+
+x = 10
+y = 20  
+print(x == y)  # => False
+print(x != y)  # => True   
+print(x > y)   # => False
+print(x < y)   # => True
+print(x >= y)  # => False
+print(x <= y)  # => True
+
