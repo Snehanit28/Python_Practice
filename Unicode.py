3f580bd8e31949dbbcabb90d1f21e7bd13d7684e
Sneha Saha <souvik9874721150@gmail.com>
2026-02-04 00:00:58 +0530

    Unicoder

---
+++ Unicode.py
@@ -0,0 +1,19 @@
+# String and Type Conversion in Python
+#unicode values
+print(ord('A'))  # => 65
+print(chr(65))     # => 'A'
+
+print(ord('a'))  # => 97
+print(chr(97))     # => 'a'
+
+print(ord('@'))  # => 64
+print(chr(64))     # => '@'
+
+print(ord('😊'))  # => 128522
+print(chr(128522)) # => '😊'
+
+print(ord('👍')) # => 128077
+print(chr(128077)) # => '👍'
+
+# print(ord('Sneha Saha'))  
+# TypeError: ord() expected a character, but string of length 10 found
